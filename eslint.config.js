f5e3e1a1feb9c31923c89af28c2b582e23768b1a
krtp0028 <krtp0028@users.noreply.github.com>
2026-09-15 23:01:44 +0700

    build: add eslint, prettier, and vitest tooling

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist/**", "node_modules/**", "src-tauri/**"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+);
